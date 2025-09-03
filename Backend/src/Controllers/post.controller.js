ead6a6d07820edbd6cd867118bec96799873f43f
Gaurav Moghe <gauravmoghe0709@gmail.com>
2025-09-03 20:39:38 +0530

    imagekit setup and save a data in database

--- Backend/src/Controllers/post.controller.js
+++ Backend/src/Controllers/post.controller.js
@@ -1,5 +1,7 @@
 const postmodel = require("../models/post.model")
 const generatecaption = require("../service/Ai.service")
+const uploadfile = require ("../service/Storage.service")
+const { v4: uuidv4} = require ("uuid")
 
 
 
@@ -10,10 +12,20 @@ async function createpost(req, res) {
     const base64image = new Buffer.from(file.buffer).toString("base64") // convert buffer file to base64image file
 
     const caption = await generatecaption(base64image)
+    const result = await uploadfile(file.buffer,`${uuidv4()}`)
 
-        res.json({
-            caption
+
+        const post = await postmodel.create({
+                caption:caption,
+                Image:result.url,
+                user: req.user._id
         })
+
+        res.status(201).json({
+            message:"Post created...",
+            post
+        })
+
 
 }
 
